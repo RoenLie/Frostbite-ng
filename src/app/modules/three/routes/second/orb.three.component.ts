a9e21f2664d810287d5c4fbea7e7f77ddf60b2be
Kristoffer Roen-Lie <kristofferroenlie@gmail.com>
2021-04-10 18:05:30 +0200

    orb animation component mouse lerp improved and added touch interaction.

--- src/app/modules/three/routes/second/orb.three.component.ts
+++ src/app/modules/three/routes/second/orb.three.component.ts
@@ -31,15 +31,17 @@ import Stats from "three/examples/jsm/libs/stats.module";
     outline: none;
   }`]
 } )
-export class OrbThreeComponent implements OnInit, AfterViewInit {
+export class OrbThreeComponent implements AfterViewInit {
    @ViewChild( "stats" ) statsEl: ElementRef;
    @ViewChild( "canvas" ) canvasEl: ElementRef;
    host: Element;
    hostRect: ClientRect;
    camera: PerspectiveCamera;
-   scene: Scene = new THREE.Scene();
    renderer: WebGLRenderer;
    stats: Stats;
+   clock = new THREE.Clock();
+   mouse = new THREE.Vector2();
+   scene: Scene = new THREE.Scene();
    manager: LoadingManager = new THREE.LoadingManager();
    statsEnabled = false;
    textureInformation: any[] = [
@@ -47,35 +49,19 @@ export class OrbThreeComponent implements OnInit, AfterViewInit {
       { "file": "assets/threejs/textures/sprite1.png", "name": "sprite1" },
       { "file": "assets/threejs/textures/sprite1.png", "name": "sprite2" }
    ];
-   backgroundShader: {
+   backgroundShader: any = {
       path: {
          fragment: "assets/threejs/shaders/BackgroundFragmentShader.frag",
-         vertex: "assets/threejs/shaders/BackgroundVertexShader.vert";
-      };
+         vertex: "assets/threejs/shaders/BackgroundVertexShader.vert"
+      }
    };
    textures: any = {};
-   mouse = new THREE.Vector2();
-   clock = new THREE.Clock();
    backgroundPlane: Mesh;
    backgroundUniforms: any;
    intensity = 1.0;
-   modelsLoaded: boolean;
-   backgroundFragmentShader = "assets/threejs/shaders/BackgroundFragmentShader.frag";
-   backgroundVertexShader = "assets/threejs/shaders/BackgroundVertexShader.vert";
 
    constructor ( public elementRef: ElementRef<Element>, public rd: Renderer2 ) { };
-   ngOnInit() { }
    ngAfterViewInit() { this.init(); }
-
-   loader = {
-      show: () => { console.log( "show loader" ); },
-      hide: () => { console.log( "hide loader" ); },
-      update: ( value: any ) => {
-         value = Math.round( value * 100 ) / 100;
-         console.log( "loader progress: ", value * 100 + "%" );
-      }
-   };
-
    init() {
       this.host = this.elementRef.nativeElement;
       this.syncHostRect();
@@ -85,7 +71,12 @@ export class OrbThreeComponent implements OnInit, AfterViewInit {
       this.camera.position.set( 0, 0, 50 );
 
       // switch to false anatialis and transparent as needed
-      this.renderer = new THREE.WebGLRenderer( { canvas: this.canvasEl.nativeElement, antialias: true, alpha: true } );
+      this.renderer = new THREE.WebGLRenderer( {
+         canvas: this.canvasEl.nativeElement,
+         antialias: true,
+         alpha: true
+      } );
+
       this.renderer.setSize( this.hostRect.width, this.hostRect.height );
       this.renderer.debug.checkShaderErrors = false;
 
@@ -105,115 +96,89 @@ export class OrbThreeComponent implements OnInit, AfterViewInit {
 
       // loading manager
       this.manager.onProgress = ( item: string, loaded: number, total: number ) => {
-         this.loader.update( loaded / total );
+         const value = Math.round( loaded / total * 100 ) / 100;
+         console.log( "loader progress: ", value * 100 + "%" );
       };
 
       this.manager.onLoad = () => {
+         console.time( "managerLoad" );
+         const fragment = this.backgroundShader.path.fragment;
+         const vertex = this.backgroundShader.path.vertex;
+
+         if ( !fragment || !vertex ) return;
+
          // add objects to scene and set initial 3D states
          this.initObjects();
 
          // load background shader
-         if ( !this.backgroundFragmentShader || !this.backgroundVertexShader ) return;
+         this.shaderLoader( vertex, fragment );
 
-         this.shaderLoader( this.backgroundVertexShader, this.backgroundFragmentShader );
+         console.timeEnd( "managerLoad" );
       };
 
       // load textures
       const loader = new THREE.TextureLoader( this.manager );
       this.textureInformation.forEach( ( t: any ) => {
-         loader.load( t.file, ( object ) => {
-            this.textures[ t.name ] = object;
+         loader.load( t.file, ( obj ) => {
+            this.textures[ t.name ] = obj;
          } );
       } );
    };
-
    initObjects() {
-      console.log( "init webgl objects" );
-
-      // ambient sprites
-      const geometry = new THREE.BufferGeometry();
+      console.time( "initObjects" );
       const vertices = [];
+      const spriteCount = 900;
+      for ( let i = 0; i < spriteCount; i++ ) {
+         const x = Math.random() * 60 - 30;
+         const y = Math.random() * 60 - 30;
+         const z = Math.random() * 60 - 30;
+         vertices.push( x, y, z );
+      }
+
+      const geometry = new THREE.BufferGeometry();
+      geometry.setAttribute( "position", new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+
       const materials = [];
       const parameters = [
          [ [ 0.3, 0.7, 0.9 ], this.textures[ "sprite1" ], .3 ],
          [ [ 0.3, 0.3, 0.8 ], this.textures[ "sprite2" ], .3 ]
       ];
 
-      for ( let i = 0; i < 350; i++ ) {
-         const x = Math.random() * 60 - 30;
-         const y = Math.random() * 60 - 30;
-         const z = Math.random() * 60 - 30;
-         vertices.push( x, y, z );
-      }
-      geometry.setAttribute( "position", new THREE.Float32BufferAttribute( vertices, 3 ) );
-
       for ( let i = 0; i < parameters.length; i++ ) {
-         const color = parameters[ i ][ 0 ];
          const sprite = parameters[ i ][ 1 ];
+         const color = parameters[ i ][ 0 ];
          const size = parameters[ i ][ 2 ];
-         materials[ i ] = new THREE.PointsMaterial( { size: size, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent: true, opacity: .35 } );
+
+         materials[ i ] = new THREE.PointsMaterial( {
+            size: size,
+            map: sprite,
+            blending: THREE.AdditiveBlending,
+            depthTest: false,
+            transparent: true,
+            opacity: .35
+         } );
          materials[ i ].color.setRGB( color[ 0 ], color[ 1 ], color[ 2 ] );
+
          const particles = new THREE.Points( geometry, materials[ i ] );
          particles.rotation.x = Math.random() * 6;
          particles.rotation.y = Math.random() * 6;
          particles.rotation.z = Math.random() * 6;
+
          this.scene.add( particles );
       }
 
       // start animating
       this.render();
+
+      console.timeEnd( "initObjects" );
    }
-
-   render = () => {
-      const ready = this.scene
-         && this.camera
-         && this.backgroundUniforms;
-
-      if ( !ready ) {
-         requestAnimationFrame( this.render );
-         return;
-      }
-
-      const delta = this.clock.getDelta();
-
-      this.backgroundUniforms.iTime.value += delta;
-      this.backgroundUniforms.audio1.value = 128.0 / 48.0 + Math.random() * .1;
-      this.backgroundUniforms.iMouse.value = this.mouse;
-      this.backgroundUniforms.intensity.value = this.intensity;
-
-      for ( let i = 0; i < this.scene.children.length; i++ ) {
-         const object = this.scene.children[ i ];
-         const isInstanceOf = object instanceof THREE.Points;
-         if ( !isInstanceOf ) continue;
-
-         object.rotation.z = -.03 * this.backgroundUniforms.iTime.value * ( i < 4 ? i + 1 : - ( i + 1 ) );
-      }
-
-      const vector = new THREE.Vector3(
-         ( -this.mouse.x * .01 - this.camera.position.x ) * .05,
-         ( this.mouse.y * .01 - this.camera.position.y ) * .05,
-         1
-      );
-
-      // this formula makes the lerp framerate independant
-      const alpha = 1.0 - Math.pow( 0.001, delta );
-      this.camera.position.lerp( vector, alpha );
-
-      this.renderer.render( this.scene, this.camera );
-
-      if ( this.statsEnabled ) { this.stats.update(); }
-
-      requestAnimationFrame( this.render );
-   };
-
    createBackgroundShader( vertex: any, fragment: any ) {
-      console.log( "background shader loaded" );
-
-      // create a bakgorund shader plane;
+      // create a background shader plane;
       this.backgroundUniforms = {
          iTime: { type: "f", value: 100.0 },
          iResolution: { type: "v2", value: new THREE.Vector2() },
-         iMouse: { type: "v2", value: new THREE.Vector2() },
+         iMouse: { type: "v2", value: new THREE.Vector3() },
          audio1: { type: "f", value: 0.0 },
          adj: { type: "f", value: 0.0 },
          orbOpacity: { type: "f", value: 1.0 },
@@ -238,13 +203,69 @@ export class OrbThreeComponent implements OnInit, AfterViewInit {
 
       const geometry = new PlaneGeometry( 1, 1 );
       this.backgroundPlane = new THREE.Mesh( geometry, material );
-      this.backgroundPlane = new THREE.Mesh( geometry, material );
       this.backgroundPlane.scale.set( 110 * aspect, 110, 1 );
 
       this.scene.add( this.backgroundPlane );
-
-      this.modelsLoaded = true;
    }
+   shaderLoader( vertex_url: string, fragment_url: string ) {
+      console.time( "shaderLoader" );
+
+      const vertex_loader = new THREE.FileLoader( THREE.DefaultLoadingManager );
+      vertex_loader.setResponseType( 'text' );
+
+      vertex_loader.load( vertex_url, ( vertex_text ) => {
+         console.time( "vertexLoader" );
+
+         const fragment_loader = new THREE.FileLoader( THREE.DefaultLoadingManager );
+         fragment_loader.setResponseType( 'text' );
+         fragment_loader.load( fragment_url, ( fragment_text ) => {
+            console.time( "fragmentLoader" );
+            this.createBackgroundShader( vertex_text, fragment_text );
+            console.timeEnd( "fragmentLoader" );
+         } );
+
+         console.timeEnd( "vertexLoader" );
+      } );
+      console.timeEnd( "shaderLoader" );
+   }
+   render = () => {
+      const ready = this.scene && this.camera && this.backgroundUniforms;
+      if ( !ready ) { requestAnimationFrame( this.render ); return; }
+
+      const delta = this.clock.getDelta();
+      const alpha = 1.0 - Math.pow( 0.001, delta / 5 );
+
+      const mouseVector = new THREE.Vector3(
+         this.mouse.x, this.mouse.y, 1 );
+
+      const camVectorModifier = .05;
+      const camVector = new THREE.Vector3(
+         ( -this.mouse.x * camVectorModifier - this.camera.position.x ) * camVectorModifier,
+         ( this.mouse.y * camVectorModifier - this.camera.position.y ) * camVectorModifier,
+         1
+      );
+
+      this.camera.position.lerp( camVector, alpha );
+
+      this.backgroundUniforms.iMouse.value.lerp( mouseVector, alpha );
+      this.backgroundUniforms.iTime.value += delta;
+      this.backgroundUniforms.audio1.value = 128.0 / 48.0 + Math.random() * .1;
+      this.backgroundUniforms.intensity.value = this.intensity;
+
+      for ( let i = 0; i < this.scene.children.length; i++ ) {
+         const obj = this.scene.children[ i ];
+         if ( !( obj instanceof THREE.Points ) ) continue;
+
+         obj.rotation.z = -.03 * this.backgroundUniforms.iTime.value * ( i < 4 ? i + 1 : - ( i + 1 ) );
+      }
+
+      this.renderer.render( this.scene, this.camera );
+
+      if ( this.statsEnabled ) { this.stats.update(); }
+
+      requestAnimationFrame( this.render );
+   };
+   syncHostRect = () => this.hostRect = this.host.getBoundingClientRect();
 
    @HostListener( "mousemove", [ "$event" ] )
    mouseMove( e: MouseEvent ) {
@@ -252,8 +273,25 @@ export class OrbThreeComponent implements OnInit, AfterViewInit {
       this.mouse.y = ( e.clientY - this.hostRect.height / 2 );
    }
 
+   @HostListener( "mouseout", [ "$event" ] )
+   mouseOut( e?: MouseEvent ) {
+      this.mouse.x = this.hostRect.width / 2;
+      this.mouse.y = this.hostRect.height / 2;
+   }
+
+   @HostListener( "touchmove", [ "$event" ] )
+   touchMove( e: TouchEvent ) {
+      this.mouse.x = ( e.touches[ 0 ].clientX - this.hostRect.width / 2 );
+      this.mouse.y = ( e.touches[ 0 ].clientY - this.hostRect.height / 2 );
+   }
+
+   @HostListener( "touchend", [ "$event" ] )
+   touchEnd( e: TouchEvent ) {
+      this.mouseOut();
+   }
+
    @HostListener( "window:resize", [ "$event" ] )
-   resize( event?: Event ) {
+   resize( e?: Event ) {
       this.syncHostRect();
       const width = this.hostRect.width;
       const height = this.hostRect.height;
@@ -265,19 +303,4 @@ export class OrbThreeComponent implements OnInit, AfterViewInit {
       this.backgroundUniforms.iResolution.value.y = height;
       this.backgroundUniforms.adj.value = .2 - this.hostRect.height / this.hostRect.width;
    }
-
-   shaderLoader( vertex_url: string, fragment_url: string, onLoad?: any, onProgress?: any, onError?: any ) {
-      const vertex_loader = new THREE.FileLoader( THREE.DefaultLoadingManager );
-      vertex_loader.setResponseType( 'text' );
-
-      vertex_loader.load( vertex_url, ( vertex_text ) => {
-         const fragment_loader = new THREE.FileLoader( THREE.DefaultLoadingManager );
-         fragment_loader.setResponseType( 'text' );
-         fragment_loader.load( fragment_url, ( fragment_text ) => {
-            this.createBackgroundShader( vertex_text, fragment_text );
-         } );
-      }, onProgress, onError );
-   }
-
-   syncHostRect = () => this.hostRect = this.host.getBoundingClientRect();
 }
